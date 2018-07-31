52658fc619fd24e512ee5c7589c266eaab317cae
MARIO_NASSEF <developer.mario.nassef@gmail.com>
2018-07-31 18:34:56 +0200

    before we push in mario branch

---
+++ src/RegistrationModuleServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Clueapp\UserPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class RegistrationModuleServiceProvider extends ServiceProvider {
+
+    public function boot() {
+        
+       /*
+       *--------------------------------------------------------------------------
+       * Publishes For Package In Installation
+       *--------------------------------------------------------------------------
+       *
+       * This will be your files when copied in their directory
+       *
+       */
+       
+//        $this->publishes([
+//            __DIR__ . '/config/' => config_path('contact.php')
+//        ]);
+//
+        $this->publishes([
+            __DIR__ . '/Http/Controllers/' => base_path('/app/Http/Controllers/')
+        ]);
+    
+        $this->publishes([
+            __DIR__ . '/database/migrations/' => database_path('migrations')
+        ]);
+        
+//        $this->publishes([
+//            __DIR__ . '/views/' => base_path('/resources/views/')
+//        ]);
+    
+        $this->publishes([
+            __DIR__ . '/Models/' => base_path('app/')
+        ]);
+    
+        
+        $this->publishes([
+            __DIR__ . '/Helper/' => base_path('/app/Http/Controllers/')
+        ]);
+    
+//        $this->publishes([
+//            __DIR__ . '/routes/' => base_path('routes/')
+//        ]);
+        
+    }
+    
+    public function register() {
+    
+    }
+}
